840a5e81d80e483135a9c4e3f1dee4ca18a7d748
michellamilecka <98780816+michellamilecka@users.noreply.github.com>
2025-11-28 12:08:09 +0100

    created basic entities

---
+++ prisma/migrations/20251128105447_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Report" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id_object" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "added_date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Opinion" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id_object" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "stars" INTEGER NOT NULL,
+    "added_date" DATETIME NOT NULL
+);
